f4e1a1122ffdad309893ecb30ace07e62924002f
JaewoongMoon <mjw8585@gmail.com>
2017-06-01 14:58:11 +0900

    fix lol test

--- xxe/lol_test.php
+++ xxe/lol_test.php
@@ -2,14 +2,14 @@
 
 $s0 = <<<XML
 	<!DOCTYPE test
-	[<!ENTITY lol "lollollollollollol">]>
+	[<!ENTITY lol "lollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollol">]>
 	<test><testing>
 	foo
 	</testing></test>
 XML;
 
 $xml = $s0;
-$until = 500;
+$until = 10000;
 $acc = "";
 
 for($i=0; $i < $until; $i++){
@@ -17,8 +17,12 @@ for($i=0; $i < $until; $i++){
 }
 $xml = str_replace("foo", $acc, $s0);
 
+
+// entity loading disabled
+libxml_disable_entity_loader(true);
+
 $dom = new DOMDocument();
-$dom->loadXML($xml, LIBXML_NOENT | LIBXML_DTDLOAD);
+$dom->loadXML($xml);
 $doc = simplexml_import_dom($dom);
 echo $doc->testing;
 
